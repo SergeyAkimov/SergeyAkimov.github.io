44b89f1094b99472fefee251f34ffca9fafa375b
unknown <makuta5480@mail.ru>
2019-08-03 15:41:54 +0300

    gulp project

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCss = require('gulp-clean-css');
+
+const cssFiles = [
+    './node_modules/normalize.css/normalize.css',
+    './src/css/some.css',
+    './src/css/other.css'
+];
+
+function styles(){
+    return gulp.src(cssFiles)
+                .pipe(concat('all.css'))
+                .pipe(autoprefixer({
+                    browsers: ['>0.1%'],
+                    cascade: false
+                }))
+                .pipe(cleanCss({level: 2}))
+                .pipe(gulp.dest('./build/css'));
+}
+function scripts(){
+
+}
+gulp.task('styles', styles);
+gulp.task('scripts', scripts);
