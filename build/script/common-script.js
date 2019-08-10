3156c22559e305a51d34570d8dff5275797862f3
SergeyAkimov <makuta5480@mail.ru>
2019-08-10 20:21:51 +0300

    test snapsvg

--- build/script/common-script.js
+++ build/script/common-script.js
@@ -1,1 +1,38 @@
-"use strict";var svgWidth=document.querySelector("#svg").clientWidth,svgHeight=document.querySelector("#svg").clientHeight,s=Snap("#svg"),makeCoords=function(e,t){return e/2+(1<arguments.length&&void 0!==t?t:0)},colorChange=function(){return circle.animate({fill:"red"},300)},circle=s.circle(makeCoords(svgWidth),makeCoords(svgHeight),makeCoords(svgWidth-5));circle.attr({fill:"transparent",stroke:"#a00",strokeWidth:5,strokeDasharray:927,strokeDashoffset:927}).transform("rotate(-90deg)"),circle.node.onclick=function(){colorChange(),document.querySelector("#message").innerHTML="Вы нажали на круг!"},setTimeout(function(){Snap.animate(927,0,function(e){circle.attr({strokeDashoffset:e})},500,mina.easeout)},500);
+"use strict";
+
+var svgWidth = document.querySelector('#svg').clientWidth,
+    svgHeight = document.querySelector('#svg').clientHeight,
+    s = Snap('#svg'),
+    //дейсвтия над svg
+makeCoords = function makeCoords(valueCenter) {
+  var offset = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+  return valueCenter / 2 + offset;
+},
+    colorChange = function colorChange() {
+  return circle.animate({
+    fill: '#a00'
+  }, 300);
+}; //рисуем svg
+
+
+var circle = s.circle(makeCoords(svgWidth), makeCoords(svgHeight), makeCoords(svgWidth - 5));
+
+circle.attr({
+  fill: 'transparent',
+  stroke: '#a00',
+  strokeWidth: 5,
+  strokeDasharray: 927,
+  strokeDashoffset: 927
+}).transform('rotate(-90deg)').node.onclick = function () {
+  colorChange();
+  document.querySelector('#message').innerHTML = 'Вы нажали на круг!';
+}; //анимируем
+
+
+setTimeout(function () {
+  Snap.animate(927, 0, function (val) {
+    circle.attr({
+      strokeDashoffset: val
+    });
+  }, 500, mina.easeout); // circle.animate({strokeDashoffset: 927});
+}, 500);
--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,25 @@
 const svgWidth = document.querySelector('#svg').clientWidth,
         svgHeight = document.querySelector('#svg').clientHeight,
-        s = Snap('#svg');
-
-//подсчитать координаты или радиус
-const makeCoords=(valueCenter, offset=0)=> valueCenter/2 + offset,
-        colorChange=()=>circle.animate({fill: 'red'}, 300)
+        s = Snap('#svg'),
+        //дейсвтия над svg
+        makeCoords=(valueCenter, offset=0)=> valueCenter/2 + offset,
+        colorChange=()=>circle.animate({fill: '#a00'}, 300);
 
 //рисуем svg
 let circle = s.circle(makeCoords(svgWidth), makeCoords(svgHeight), makeCoords(svgWidth-5));
 
-circle.attr({
-    fill: 'transparent',
-    stroke: '#a00',
-    strokeWidth: 5,
-    strokeDasharray: 927,
-    strokeDashoffset: 927
-}).transform('rotate(-90deg)');
-//по клику покрасим и выведем сообщение
-circle.node.onclick =()=> {
-    colorChange();
-    document.querySelector('#message').innerHTML = 'Вы нажали на круг!'
-}
+circle.attr({ 
+        fill: 'transparent',
+        stroke: '#a00',
+        strokeWidth: 5,
+        strokeDasharray: 927,
+        strokeDashoffset: 927
+    })
+    .transform('rotate(-90deg)')
+    .node.onclick =()=> {
+        colorChange();
+        document.querySelector('#message').innerHTML = 'Вы нажали на круг!'
+    };
 
 //анимируем
 setTimeout(()=>{
