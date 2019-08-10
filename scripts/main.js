c708ef154d106cda79706860bef85ca047b0f0fa
SergeyAkimov <makuta5480@mail.ru>
2019-08-10 15:02:01 +0300

    test snapsvg

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,21 @@
-const sum =(a,b)=>a+b;
-console.log('Hello world');
+const svgWidth = document.querySelector('#svg').clientWidth,
+        svgHeight = document.querySelector('#svg').clientHeight,
+        s = Snap('#svg');
+
+//подсчитать координаты или радиус
+makeCoords=(valueCenter, offset=0)=> valueCenter/2 + offset
+
+//рисуем svg
+let circle = s.circle(makeCoords(svgWidth), makeCoords(svgHeight), makeCoords(svgWidth));
+circle.attr({
+    fill: 'none',
+    stroke: '#a00',
+    strokeWidth: 5,
+    strokeDasharray: 1571,
+    // strokeDashoffset: 1571
+    
+});
+Snap.animate(1571, 0, function(val){
+    circle.attr({strokeDashoffset: val});
+}, 1000, mina.easeout);
+circle.animate({strokeDashoffset: 0});
